1fcac8e5b048c66452e475a4f47f08ad0ca98c9b
Colin <ricec2@rpi.edu>
2012-05-26 20:18:40 -0700

    Figured out upload method

--- frontend/upload/UploadPage.py
+++ frontend/upload/UploadPage.py
@@ -1,6 +1,9 @@
 import flask
 from frontend import app
 
-@app.route("/upload")
+@app.route("/upload", methods=['GET', 'POST'])
 def UploadPage():
+
+    print flask.request.form
+    print flask.request.files
     return flask.render_template('upload/upload.html')
