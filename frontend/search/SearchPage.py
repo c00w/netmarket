456c52144ceb03cc72d7043ea89e24eebb4fcade
Colin <ricec2@rpi.edu>
2012-05-26 19:56:22 -0700

    Make search page load

--- frontend/search/SearchPage.py
+++ frontend/search/SearchPage.py
@@ -1,6 +1,7 @@
 from frontend import app
 import flask
 
-app.route("/search", methods=["POST", "GET"])
+@app.route("/search", methods=["POST", "GET"])
 def search():
+    print 'search'
     return flask.render_template("search/search.html")
