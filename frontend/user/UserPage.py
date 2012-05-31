d561fa135c73629ab6da5f3e1198183b45aa2e85
Colin <ricec2@rpi.edu>
2012-05-30 22:12:30 -0700

    Update login page

--- frontend/user/UserPage.py
+++ frontend/user/UserPage.py
@@ -1,15 +1,16 @@
 from frontend import app
 from flask.ext.login import login_required
+import flask
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    if validate_log_post(flask.request.form): 
+    if validate_login_post(flask.request.form): 
         # login and validate the user...
         login_user(user)
         flash("Logged in successfully.")
         return redirect(request.args.get("next") or url_for("/"))
     
-    return render_template("user/login.html", form=form)
+    return flask.render_template("user/login.html")
 
 def validate_login_post(header):
     print header
