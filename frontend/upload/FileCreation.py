35689fa55a6f096ba8e04ab3a5b0b932d4150e45
Colin <ricec2@rpi.edu>
2012-06-06 21:06:26 -0700

    A little more work on file uploads

--- frontend/upload/FileCreation.py
+++ frontend/upload/FileCreation.py
@@ -3,6 +3,8 @@ import flask
 
 @app.route("/upload/create", methods=['GET', 'POST'])
 def upload_create():
+    print flask.request
+    print flask.request.form
     return flask.render_template("upload/create.html")
 
 def handle_post():
--- frontend/upload/FileUpload.py
+++ frontend/upload/FileUpload.py
@@ -1,7 +1,11 @@
 import flask
 from frontend import app
+from frontend.page_util import has_fields
 
 @app.route("/upload/upload", methods=['GET', 'POST'])
 def UploadPage():
 
+    if 'data' in flask.request.files:
+        print flask.request.files['data']  
     return flask.render_template('upload/upload.html')
+
