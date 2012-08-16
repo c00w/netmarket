0063be8b6ffcd5283dccf60b103864f9f091f000
Colin <ricec2@rpi.edu>
2012-08-16 02:23:32 -0400

    Work on actually making files page usefull

--- frontend/files/FilesPage.py
+++ frontend/files/FilesPage.py
@@ -2,8 +2,15 @@ from frontend import app
 import flask
 from flask.ext.login import login_required, current_user
 
+import simplejson as json
+
 @app.route("/files")
 @login_required
 def files():
-    files = current_user.get_files() 
     return flask.render_template("files/files.html")
+
+@app.route("/files/list")
+@login_required
+def list_files():
+    files = current_user.get_files() 
+    return json.dumps(files)
